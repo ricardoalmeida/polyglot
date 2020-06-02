83044f4cf6d1bd991ef996aeb524a35624ea9b07
Ricardo Almeida <ricardoluiz1.almeida@gmail.com>
2020-06-02 20:03:19 +0200

    Number in list in Go

---
+++ numinlist/num_in_list.go
@@ -0,0 +1,12 @@
+package numinlist
+
+// NumInList will return true if num is in the
+// list slice, and false otherwise.
+func NumInList(list []int, num int) bool {
+	for _, n := range list {
+		if n == num {
+			return true
+		}
+	}
+	return false
+}
