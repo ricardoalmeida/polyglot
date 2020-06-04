9b65f4c676e357cf9a6f52f57d7f51e029e3d56d
Ricardo Almeida <ricardoluiz1.almeida@gmail.com>
2020-06-04 11:30:06 +0200

    Add src folder to elixir

--- mix.exs
+++ mix.exs
@@ -7,9 +7,12 @@ defmodule Polyglot.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
     ]
   end
+
+  defp elixirc_paths(_), do: ["src"]
 
   def application do
     [
