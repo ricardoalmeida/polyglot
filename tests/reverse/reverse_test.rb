7ee1d7cc9c6b5750c418ee3db7685d6046bea871
Ricardo Almeida <ricardoluiz1.almeida@gmail.com>
2020-06-09 18:41:30 +0200

    Add FizzBuzz in Ruby

--- tests/reverse/reverse_test.rb
+++ tests/reverse/reverse_test.rb
@@ -4,21 +4,35 @@ require 'test_helper'
 require 'reverse/reverse'
 
 describe Reverse do
+  def setup
+    @tests = [
+      { word: 'cat', want: 'tac' },
+      { word: 'alphabet', want: 'tebahpla' },
+      { word: '日本語', want: '語本日' }
+    ]
+  end
+
   describe '.iterate' do
     it 'reverses a word' do
-      _(Reverse.iterate('opa')).must_equal('apo')
+      @tests.each do |tc|
+        _(Reverse.iterate(tc[:word])).must_equal(tc[:want])
+      end
     end
   end
 
   describe '.recursive' do
     it 'reverses a word' do
-      _(Reverse.recursive('opa')).must_equal('apo')
+      @tests.each do |tc|
+        _(Reverse.recursive(tc[:word])).must_equal(tc[:want])
+      end
     end
   end
 
   describe '.std' do
     it 'reverses a word' do
-      _(Reverse.std('opa')).must_equal('apo')
+      @tests.each do |tc|
+        _(Reverse.std(tc[:word])).must_equal(tc[:want])
+      end
     end
   end
 end
