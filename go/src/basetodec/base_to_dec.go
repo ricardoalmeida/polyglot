f3ae260048c192f6b89c190e86697185ca0f93d2
Ricardo Almeida <ricardoluiz1.almeida@gmail.com>
2020-06-12 19:09:11 +0200

    Add Base to dec in Go

---
+++ go/src/basetodec/base_to_dec.go
@@ -0,0 +1,41 @@
+package basetodec
+
+import "fmt"
+
+// https://github.com/joncalhoun/algorithmswithgo.com/tree/master/module01#10---another-base-to-decimal-code
+
+const charset = "0123456789ABCDEF"
+
+// Iterate :
+func Iterate(value string, base int) int {
+	res := 0
+	multiplier := 1
+	for i := len(value) - 1; i >= 0; i-- {
+		index := -1
+		for j, char := range charset {
+			if char == rune(value[i]) {
+				index = j
+				break
+			}
+		}
+		if index < 0 {
+			panic("something went wong!")
+		}
+		res += index * multiplier
+		multiplier *= base
+	}
+	return res
+}
+
+// Std :
+func Std(value string, base int) int {
+	res := 0
+	multiplier := 1
+	for i := len(value) - 1; i >= 0; i-- {
+		var val int
+		fmt.Sscanf(string(value[i]), "%X", &val)
+		res += multiplier * val
+		multiplier *= base
+	}
+	return res
+}
---
+++ go/tests/basetodec/base_to_dec_test.go
@@ -0,0 +1,56 @@
+package basetodec_test
+
+import (
+	"fmt"
+	"github/ricardoalmeida/polyglot/src/basetodec"
+	"testing"
+)
+
+func TestIterate(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%v in base %v", tc.have, tc.base), func(t *testing.T) {
+			got := basetodec.Iterate(tc.have, tc.base)
+			if got != tc.want {
+				t.Fatalf("Iterate() = %v; want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestStd(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%v in base %v", tc.have, tc.base), func(t *testing.T) {
+			got := basetodec.Std(tc.have, tc.base)
+			if got != tc.want {
+				t.Fatalf("Std() = %v; want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+var tests = []struct {
+	want, base int
+	have       string
+}{
+	{1, 2, "1"},
+	{2, 2, "10"},
+	{7, 3, "21"},
+	{14, 2, "1110"},
+	{14, 16, "E"},
+	{17, 16, "11"},
+	{3735928559, 2, "11011110101011011011111011101111"},
+	{3735928559, 3, "100122100210211112102"},
+	{3735928559, 4, "3132223123323233"},
+	{3735928559, 5, "30122344203214"},
+	{3735928559, 6, "1414413525315"},
+	{3735928559, 7, "161402603666"},
+	{3735928559, 8, "33653337357"},
+	{3735928559, 9, "10570724472"},
+	{3735928559, 10, "3735928559"},
+	{3735928559, 11, "164791A470"},
+	{3735928559, 12, "8831A383B"},
+	{3735928559, 13, "476CC321C"},
+	{3735928559, 14, "276253DDD"},
+	{3735928559, 15, "16CEB1BDE"},
+	{3735928559, 16, "DEADBEEF"},
+}
